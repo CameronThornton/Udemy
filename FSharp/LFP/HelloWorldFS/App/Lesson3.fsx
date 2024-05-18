32852692daa81c43f0c5fe9a324766d27eba6873
cap coldman <moctopus@outlook.com>
2024-05-18 10:46:52 -0400

    more f# udemy

--- FSharp/LFP/HelloWorldFS/App/Lesson3.fsx
+++ FSharp/LFP/HelloWorldFS/App/Lesson3.fsx
@@ -1,24 +1,48 @@
 module Lesson3
 
+open System
+
+[<AbstractClass>]
+type IManager() =
+    abstract member Authorize: unit -> unit
+    default __.Authorize() = ()
+
 type Employee =
-    class
-        val age: int
-        val fullName: string
-        val mutable department: string
+    inherit IManager
 
-        new(fullName, age) = Employee(fullName, age, "Other")
+    [<DefaultValue>]
+    val mutable private age: int
 
-        new(fullName, age, department) as this =
-            { fullName = fullName
-              age = age
-              department = department }
-            then this.department <- "some department"
-    end
+    val fullName: string
+    val mutable department: string
 
-type Employee2(fullName: string, age: int, department: string) =
+    member employee.Age
+        with get () = employee.age
+        and private set (age) = employee.age <- age
+
+    new(fullName, age) = Employee(fullName, age, "Other")
+
+    new(fullName, age, department) as this =
+        { fullName = fullName
+          //age = age
+          department = department }
+        then this.department <- "some department"
+
+type Employee2<'T>(fullName: string, age: int, department: string) =
+    inherit IManager()
     let x = 4
+
+    let f x y = x
+
     do printfn "%i" x
     new(fullName, age) = Employee2(fullName, age, "Other")
 
+    member val Age = 35 with get, set
+
+    member __.SomeMethod(a: 'T) = a
+
+    member this.SomeInteger(i: 'T) = this.SomeMethod i
+    override __.Authorize() = ()
+
 let emp = Employee("person", 33, "hamburger")
-let emp2 = Employee2("person2", 332, "hamburger")
+let emp2 = Employee2<int>("person2", 332, "hamburger")
